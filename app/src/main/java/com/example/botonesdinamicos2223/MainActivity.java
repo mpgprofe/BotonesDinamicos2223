3a0150ba3c0dcc4b070bed60a84dd98eb94719b3
manuel <mpgprofe@iesflorenciopintado.es>
2022-10-04 11:01:52 +0200

    borrando pares

--- app/src/main/java/com/example/botonesdinamicos2223/MainActivity.java
+++ app/src/main/java/com/example/botonesdinamicos2223/MainActivity.java
@@ -12,13 +12,17 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button bCrear,bBorrar;
+    Button bCrear,bBorrar, bBorrarPares;
     EditText nBotones;
     GridLayout tabla;
+    int[] idbotones;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,15 +32,20 @@ public class MainActivity extends AppCompatActivity {
         nBotones = findViewById(R.id.editTextNBotones);
         tabla = findViewById(R.id.layoutTabla);
         bBorrar = findViewById(R.id.buttonBorrar);
+        bBorrarPares = findViewById(R.id.buttonBorrarPares);
 
         bCrear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int cantidad = Integer.parseInt(nBotones.getText().toString());
+                idbotones = new int[cantidad];
                 for (int i=0; i < cantidad; i++){
                     Button botonAux = new Button(MainActivity.this);
                     botonAux.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                     botonAux.setText("Botón "+i);
+                    botonAux.setId(View.generateViewId());
+                    idbotones[i] = botonAux.getId();
+
                     botonAux.setBackgroundColor(dameColorAleatorio());
                     final int numero =i;
 
@@ -57,6 +66,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 tabla.removeAllViews();
+            }
+        });
+
+        bBorrarPares.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                ArrayList<View> botones = tabla.getTouchables();
+                Iterator<View> iterator = botones.iterator();
+
+                while (iterator.hasNext()){
+                    View v = iterator.next();
+                    if (v instanceof Button){
+
+                        int id = v.getId();
+                        boolean encontrado = false;
+
+
+                        for(int i = 0; i< idbotones.length && !encontrado; i++){
+                            if (i%2==0 && id==idbotones[i]){
+                                tabla.removeView(v);
+                                encontrado=true;
+
+                            }
+                        }
+                        /*
+                        String [] cadenas = ((Button) v).getText().toString().split(" ");
+                        int numero = Integer.parseInt(cadenas[1]);
+                        if (numero%2==0){
+                            tabla.removeView(v);
+                        }
+                        */
+                    }
+                }
+
+
             }
         });
 
